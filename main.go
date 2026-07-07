0a4988b40b3ba8688e9e3c8072d436d632d89ce0
DELMEERs <dlnxtrm@gmail.com>
2026-07-07 17:02:26 +0700

    refactor: extract URL checking logic into separate function

--- main.go
+++ main.go
@@ -8,37 +8,38 @@ import (
 	"time"
 )
 
+func CheckURL(urlStr string) {
+
+	if !strings.HasPrefix(urlStr, "http://") && !strings.HasPrefix(urlStr, "https://") {
+		urlStr = "http://" + urlStr
+	}
+
+	start := time.Now()
+	resp, err := http.Head(urlStr)
+	if err != nil {
+		errStr := err.Error()
+		if lastIndex := strings.LastIndex(errStr, ": "); lastIndex != -1 {
+			errStr = errStr[lastIndex+2:]
+		}
+		fmt.Printf("[error] %s - %v\n", urlStr, errStr)
+		return
+	}
+
+	duration := time.Since(start).Round(time.Millisecond)
+	resp.Body.Close()
+
+	fmt.Printf("[%d] %s - %s\n", resp.StatusCode, urlStr, duration)
+
+}
+
 func main() {
 	if len(os.Args[1:]) == 0 {
-		fmt.Println("using the application: ./downboy [website]")
+		fmt.Println("using the application: ./downboy [websites]")
 		os.Exit(1)
 	}
 
 	for _, value := range os.Args[1:] {
-
-		urlStr := value
-
-		if !strings.HasPrefix(urlStr, "http://") && !strings.HasPrefix(urlStr, "https://") {
-			urlStr = "http://" + urlStr
-		}
-
-		start := time.Now()
-
-		resp, err := http.Head(urlStr)
-		if err != nil {
-			errStr := err.Error()
-			if lastIndex := strings.LastIndex(errStr, ": "); lastIndex != -1 {
-				errStr = errStr[lastIndex+2:]
-			}
-
-			fmt.Printf("[error] %s - %v\n", value, errStr)
-			continue
-		}
-
-		duration := time.Since(start)
-		resp.Body.Close()
-
-		fmt.Printf("[%d] %s - %s\n", resp.StatusCode, value, duration.Round(time.Millisecond))
+		CheckURL(value)
 	}
 
 }
